b2ba97e1f8dc15e5c166caba006d4fcc23688ae5
Cha <57724873+chablades@users.noreply.github.com>
2025-09-08 20:16:23 -0700

    final update

--- TicTacToe/src/test/java/edu/cpp/tictactoe/BoardTest.java
+++ TicTacToe/src/test/java/edu/cpp/tictactoe/BoardTest.java
@@ -73,7 +73,7 @@ class BoardTest {
     }
 
     @Test
-    void opposite_diagonalWinDetected(){
+    void oppositeDiagonalWinDetected(){
         Board b = new Board(3, 3);
         b.place(new Move(0, 2, Mark.X));
         b.place(new Move(1, 2, Mark.O));
@@ -82,5 +82,26 @@ class BoardTest {
         b.place(new Move(2, 0, Mark.X));
         assertEquals(Optional.of(Mark.X), b.winner());
     }
+
+    @Test
+    void boardResetClearsAllCellsAndWinner() {
+        Board b = new Board(3, 3);
+        b.place(new Move(0, 0, Mark.X));
+        b.place(new Move(0, 1, Mark.X));
+        b.place(new Move(0, 2, Mark.X)); // X wins
+        assertEquals(Optional.of(Mark.X), b.winner());
+
+        b.reset();
+
+        // All cells should be empty
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                assertNull(b.getCell(r, c), "Cell (" + r + "," + c + ") should be empty after reset");
+            }
+        }
+        // Winner should be cleared
+        assertEquals(Optional.empty(), b.winner());
+    }
+
 
 }
--- TicTacToe/src/test/java/edu/cpp/tictactoe/GameTest.java
+++ TicTacToe/src/test/java/edu/cpp/tictactoe/GameTest.java
@@ -102,4 +102,69 @@ class GameTest {
         assertTrue(board.isFull());
         assertEquals(Optional.empty(), board.winner());
     }
+
+    @Test
+    void scriptedGameFourByFourWin3() {
+        Board board = new Board(4,3);
+
+        // X will win on the top row
+        ScriptedPlayer p1 = new ScriptedPlayer(Mark.X, List.of(
+                new Move(3, 0, Mark.X),
+                new Move(3, 1, Mark.X),
+                new Move(3, 2, Mark.X)
+        ));
+
+        ScriptedPlayer p2 = new ScriptedPlayer(Mark.O, List.of(
+                new Move(1, 0, Mark.O),
+                new Move(1, 1, Mark.O)
+        ));
+
+        Game game = new Game(p1, p2, board);
+        game.run();
+
+        assertEquals(Optional.of(Mark.X), board.winner());
+        assertFalse(board.isFull()); // ended early
+    }
+
+    @Test
+    void scriptedGameFourByFourWin4() {
+        Board board = new Board(4,4);
+
+        // X will win on the top row
+        ScriptedPlayer p1 = new ScriptedPlayer(Mark.X, List.of(
+                new Move(3, 0, Mark.X),
+                new Move(3, 1, Mark.X),
+                new Move(3, 2, Mark.X),
+                new Move(3, 3, Mark.X)
+        ));
+
+        ScriptedPlayer p2 = new ScriptedPlayer(Mark.O, List.of(
+                new Move(1, 0, Mark.O),
+                new Move(1, 1, Mark.O),
+                new Move(1, 2, Mark.O)
+        ));
+
+        Game game = new Game(p1, p2, board);
+        game.run();
+
+        assertEquals(Optional.of(Mark.X), board.winner());
+        assertFalse(board.isFull()); // ended early
+    }
+
+    @Test
+    void invalidMoveThrowsException() {
+        Board board = new Board(3,3);
+        ScriptedPlayer p1 = new ScriptedPlayer(Mark.X, List.of(
+                new Move(0, 0, Mark.X),
+                new Move(0, 0, Mark.X) // same spot
+        ));
+        ScriptedPlayer p2 = new ScriptedPlayer(Mark.O, List.of(
+                new Move(1, 1, Mark.O)
+        ));
+        Game game = new Game(p1, p2, board);
+        assertThrows(IllegalStateException.class, game::run);
+    }
+
+
+
 }
